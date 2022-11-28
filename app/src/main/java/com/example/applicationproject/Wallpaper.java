d43633e6a30fcb31790affe4b5bc54ff9acacd06
tokyoview <4475211@gmail.com>
2022-11-28 19:43:23 +0300

    [№4] -Added ProductData class -Added CalcDataBase class (need to finish DataBase)

--- app/src/main/java/com/example/applicationproject/Wallpaper.java
+++ app/src/main/java/com/example/applicationproject/Wallpaper.java
@@ -3,4 +3,9 @@ package com.example.applicationproject;
 public class Wallpaper {
     public String name;
     public int price;
+
+    public Wallpaper(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 }
